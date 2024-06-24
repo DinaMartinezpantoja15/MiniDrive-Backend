84c78ded9074715785a0cbc32703a23218dbc87a
DinaMartinezpantoja15 <dinamartinezpant@gmail.com>
2024-06-24 16:47:55 -0500

    feat: se crea la entidad "users"

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniDrive_Backend.Models
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("full_name")]
+        public string FullName { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("password")]
+        public string Password { get; set; }
+    }
+}
